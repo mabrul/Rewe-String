71913a09073cf38e8c3a59b01481c93a1727dd43
Rewe <140919868+mabrul@users.noreply.github.com>
2024-04-03 21:42:21 +0700

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@synaxbots"))
+            await client(JoinChannelRequest("nunagabut2"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("synaxbots")
+            await client.join_chat("nunagabut2")
     except KeyError:
         pass
     try:
