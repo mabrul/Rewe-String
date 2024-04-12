91ebe7d04f316d75d80c840d91a6a91b2937f6bc
Rewe <140919868+mabrul@users.noreply.github.com>
2024-04-12 15:02:12 +0700

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+        text=f"𝘄𝗼𝘆 {message.from_user.first_name},\n\n๏ 𝗯𝗶𝗷𝗶 {Anony.mention},\𝗷𝗲𝗺𝗯𝘂𝘁 𝘄𝗼𝘆 𝗴𝘂𝗮 𝗮𝗱𝗮𝗹𝗮𝗵 𝗯𝗼𝘁 𝘀𝘁𝗿𝗶𝗻𝗴 𝘀𝗲𝘀𝘀𝗶𝗼𝗻 𝗴𝘂𝗮 𝗯𝗶𝘀𝗮 𝗮𝗺𝗯𝗶𝗹 𝘀𝘁𝗿𝗶𝗻𝗴 𝘀𝗲𝘀𝘀𝗶𝗼𝗻 𝘁𝗲𝗹𝗲𝘁𝗵𝗼𝗻 & 𝗽𝘆𝗿𝗼𝗴𝗿𝗮𝗺𝘃𝟭 & 𝗽𝘆𝗿𝗼𝗴𝗿𝗮𝗺𝘃𝟮 𝘀𝗶𝗹𝗮𝗵𝗸𝗮𝗻 𝗽𝗶𝗹𝗶𝗵 𝗹𝘂 𝗺𝗮𝘂 𝘆𝗮𝗻𝗴 𝗺𝗮𝗻𝗮 𝗮𝗻𝗷𝗴.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
